a1d6024a9656a3b5a5be0c31ce5d23da78066ed7
tadeo123456 <116957724+tadeo123456@users.noreply.github.com>
2024-02-20 08:12:15 -0600

    se agrega el proyecto

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+const mem = document.querySelector('.mem');
+const menu = document.querySelector(' .menu-navegacion');
+
+console.log(mem)
+console.log(mem)
+
+mem.addEventListener('click',()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click',()=>{
+    console .log (e.target)
+})
